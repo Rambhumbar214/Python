2d9ccfc5b555a1027837c79724ec0968e1c5cf64
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-02-17 19:17:09 -0800

    Update A1SBQ1.py

--- Ram_py/A1SBQ1.py
+++ Ram_py/A1SBQ1.py
@@ -1,22 +1,14 @@
-def is_prime(num):
-    
-    if num <= 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+n = int(input("Enter an integer value n: "))  
 
-def display_primes(n):
-    primes = []
-    for i in range(2, n + 1):
-        if is_prime(i):
-            primes.append(i)
-    return primes
+primes = []  
 
+for num in range(2, n + 1):  
+    is_prime = True  
+    for i in range(2, int(num ** 0.5) + 1):  
+        if num % i == 0:  
+            is_prime = False  
+            break  
+    if is_prime:  
+        primes.append(num)  
 
-n = int(input("Enter an integer value n: "))
-
-
-primes = display_primes(n)
-print(f"Prime numbers up to {n} are: {primes}")
+print("Prime numbers up to", n, "are:", primes)
