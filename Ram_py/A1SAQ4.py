12cc3dbcec49bb496b89a84c88250b50960c1ec4
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-02-17 19:07:03 -0800

    Update A1SAQ4.py

--- Ram_py/A1SAQ4.py
+++ Ram_py/A1SAQ4.py
@@ -1,10 +1,3 @@
-def sum_of_digits(n):
-   
-    return sum(int(digit) for digit in str(n))
-
-
-n = int(input("Enter a number: "))
-
-
-result = sum_of_digits(n)
-print(f"The sum of digits is: {result}")
+n = int(input("Enter a number: "))  
+sum_digits = sum(int(digit) for digit in str(n))  
+print(f"The sum of digits is: {sum_digits}")  
