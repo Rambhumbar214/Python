35950b7016a9ae2fff8baba8eecc38ec8392ac69
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-02-24 10:16:22 +0530

    Update A1SCQ1.py

--- Ram_py/A1SCQ1.py
+++ Ram_py/A1SCQ1.py
@@ -1,17 +1,11 @@
-def sequential_search(sorted_list, target):
-  
-    for index, element in enumerate(sorted_list):
-    
-        if element == target:
-            return index
-   
-    return -1
-
-
 sorted_list = [1, 3, 5, 7, 9, 11, 13, 15]
 target = int(input("Enter the element to search for: "))
 
-index = sequential_search(sorted_list, target)
+index = -1
+for i, element in enumerate(sorted_list):
+    if element == target:
+        index = i
+        break
 
 if index != -1:
     print(f"Element {target} found at index {index}.")
