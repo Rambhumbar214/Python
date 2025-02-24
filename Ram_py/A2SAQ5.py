6a8638bfdfb7a55e4434f064e8e0663bce0af52a
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-02-24 10:23:33 +0530

    Update A2SAQ5.py

--- Ram_py/A2SAQ5.py
+++ Ram_py/A2SAQ5.py
@@ -1,13 +1,6 @@
-def swap_and_combine(str1, str2):
-    
-    return str2[:2] + str1[2:] + str1[:2] + str2[2:]
+str1 = input('Enter a string: ')
+str2 = input('Enter a string: ')
 
-
-str1 = input('enter a string')
-str2 = input('enter a string')
-
-
-result = swap_and_combine(str1, str2)
-
+result = str2[:2] + str1[2:] + str1[:2] + str2[2:]
 
 print(result)
