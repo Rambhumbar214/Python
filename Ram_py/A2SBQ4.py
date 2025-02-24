820175fc2c75908870ad0b4620b9784f18dfab17
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-02-24 10:26:02 +0530

    Update A2SBQ4.py

--- Ram_py/A2SBQ4.py
+++ Ram_py/A2SBQ4.py
@@ -1,29 +1,14 @@
-def count_word_occurrences(sentence):
-   
-    words = sentence.split()
-    
-    
-    word_count = {}
-    
-   
-    for word in words:
-       
-        word = word.lower()
-        
-  
-        if word in word_count:
-            word_count[word] += 1
-        else:
-            word_count[word] = 1
-    
-    return word_count
-
-
 sentence = input("Enter a sentence: ")
 
+words = sentence.split()
+word_count = {}
 
-word_count = count_word_occurrences(sentence)
-
+for word in words:
+    word = word.lower()
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
 
 print("Word occurrences:")
 for word, count in word_count.items():
