0992f80a46686a528d5e624f6c82afe36fb6fad5
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-02-24 10:25:03 +0530

    Update A2SBQ2.py

--- Ram_py/A2SBQ2.py
+++ Ram_py/A2SBQ2.py
@@ -1,22 +1,13 @@
-def count_digits_and_letters(input_string):
-    digits = 0
-    letters = 0
-    
-    
-    for char in input_string:
-        if char.isdigit():
-            digits += 1
-        elif char.isalpha():
-            letters += 1
-    
-    return digits, letters
-
-
 input_string = input("Enter a string: ")
 
+digits = 0
+letters = 0
 
-digits, letters = count_digits_and_letters(input_string)
-
+for char in input_string:
+    if char.isdigit():
+        digits += 1
+    elif char.isalpha():
+        letters += 1
 
 print(f"Number of digits: {digits}")
 print(f"Number of letters: {letters}")
