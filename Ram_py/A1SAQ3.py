c4ecbcfd67b873756997b1ae9041497cb8fa4fd2
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-02-24 10:12:09 +0530

    Update A1SAQ3.py

--- Ram_py/A1SAQ3.py
+++ Ram_py/A1SAQ3.py
@@ -1,18 +1,14 @@
-import sys
-
-def check_range(value):
-    if 1 <= value <= 50:
-        print("ok")
-    else:
-        print("out of range")
-
+import sys 
+#sys used for give command line argument
 
 if len(sys.argv) != 2:
     print("Please provide exactly one integer value as a command line argument.")
 else:
     try:
-       
         value = int(sys.argv[1])
-        check_range(value)
+        if 1 <= value <= 50:
+            print("ok")
+        else:
+            print("out of range")
     except ValueError:
         print("Please provide a valid integer.")
